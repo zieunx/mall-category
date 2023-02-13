b55aa15a3d23b070848567ab3d4fb083b4c43e15
zieunx <wldms_1217@naver.com>
2023-02-13 22:35:46 +0900

    feat: swagger 추가

--- src/main/java/com/mall/server/domain/category/controller/CategoryController.java
+++ src/main/java/com/mall/server/domain/category/controller/CategoryController.java
@@ -3,57 +3,44 @@ package com.mall.server.domain.category.controller;
 import com.mall.server.domain.category.dto.CategoryInfo;
 import com.mall.server.domain.category.dto.CreateCategory;
 import com.mall.server.domain.category.dto.UpdateCategory;
-import com.mall.server.domain.category.service.CategoryService;
-import lombok.RequiredArgsConstructor;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
-import java.net.URI;
 
-@RequiredArgsConstructor
-@RequestMapping("/category")
-@RestController
-public class CategoryController {
-    private final CategoryService categoryService;
+@Api(value = "Category", tags = {"Category Controller"})
+public interface CategoryController {
 
+    @ApiOperation(value = "카테고리 조회", notes = "카테고리를 조회한다. 부모 카테고리 없이 요청 시 전체가 조회된다.")
     @GetMapping
-    public ResponseEntity<CreateCategory.ResponseList> createCategory(
-            @RequestParam(required = false) Long parentId) {
-        categoryService.findCategoriesById(parentId);
+    ResponseEntity<CreateCategory.ResponseList> findCategories(
+            @ApiParam(value = "부모 카테고리 아이디", type = "Long")
+            @RequestParam(required = false)
+            Long parentId
+    );
 
-        return ResponseEntity.ok(
-                CreateCategory.ResponseList.from(categoryService.findCategoriesById(parentId))
-        );
-    }
-
+    @ApiOperation(value = "카테고리 등록", notes = "카테고리를 등록한다. 기본이 <strong>미노출</strong>이기 때문에 등록 후 노출처리가 필요하다.")
     @PostMapping
-    public ResponseEntity<?> createCategory(
-            @Valid @RequestBody CreateCategory.Request request) {
-        CategoryInfo category = categoryService.createCategory(request.getName(), request.getParentId());
+    ResponseEntity<CategoryInfo> createCategory(
+            @ApiParam(value = "카테고리 등록 요청", required = true)
+            @Valid @RequestBody CreateCategory.Request request);
 
-        URI selfLink = URI.create(ServletUriComponentsBuilder.fromCurrentRequest().toUriString());
-        return ResponseEntity.created(selfLink).body(category);
-    }
-
+    @ApiOperation(value = "카테고리 수정", notes = "카테고리를 수정한다. 계층 이동이 가능하다.")
     @PutMapping("/{categoryId}")
-    public ResponseEntity<?> updateCategory(
+    ResponseEntity<CategoryInfo> updateCategory(
+            @ApiParam(value = "대상 카테고리 아이디", type = "Long", required = true)
             @PathVariable Long categoryId,
-            @Valid @RequestBody UpdateCategory.Request request) {
-        CategoryInfo category = categoryService.updateCategory(categoryId, request);
+            @Valid @RequestBody UpdateCategory.Request request
+    );
 
-        URI selfLink = URI.create(ServletUriComponentsBuilder.fromCurrentRequest().toUriString());
-        return ResponseEntity.created(selfLink).body(category);
-    }
-
+    @ApiOperation(value = "카테고리 삭제", notes = "카테고리를 삭제한다.")
     @DeleteMapping("/{categoryId}")
-    public ResponseEntity<?> deleteCategory(
-            @PathVariable Long categoryId) {
-        categoryService.deleteCategory(categoryId);
-
-        URI selfLink = URI.create(ServletUriComponentsBuilder.fromCurrentRequest().toUriString());
-        return ResponseEntity.created(selfLink).build();
-    }
+    ResponseEntity<?> deleteCategory(
+            @ApiParam(value = "대상 카테고리 아이디", type = "Long", required = true)
+            @PathVariable Long categoryId
+    );
 
 }
--- src/main/java/com/mall/server/domain/category/dto/CategoryInfo.java
+++ src/main/java/com/mall/server/domain/category/dto/CategoryInfo.java
@@ -1,21 +1,36 @@
 package com.mall.server.domain.category.dto;
 
 import com.mall.server.domain.category.domain.Category;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@ApiModel(description = "카테고리")
 @Getter
 public class CategoryInfo {
+    @ApiModelProperty(value = "카테고리 아이디", dataType = "Long")
     private Long id;
+
+    @ApiModelProperty(value = "카테고리 이름", dataType = "String")
     private String name;
+
+    @ApiModelProperty(value = "노출 여부", dataType = "Boolean")
     private Boolean displayYn;
+
+    @ApiModelProperty(value = "생성일자", dataType = "LocalDateTime")
     private LocalDateTime createdAt;
+
+    @ApiModelProperty(value = "수정일자", dataType = "LocalDateTime")
     private LocalDateTime updatedAt;
+
+    @ApiModelProperty(value = "삭제일자", dataType = "LocalDateTime")
     private LocalDateTime deletedAt;
-    private CategoryInfo ancestors;
+
+    @ApiModelProperty(value = "자식 카테고리", dataType = "List<CategoryInfo>")
     private List<CategoryInfo> children;
 
     @Builder
